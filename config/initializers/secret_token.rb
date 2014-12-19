52beb5f5804c86b389f72cdd8a318bb3dc7135b8
davedawson <dave@iamdavedawson.com>
2014-12-19 11:18:08 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shoutit::Application.config.secret_key_base = 'b9a22de5d452b6d280e8af7680a09149180fc0eca17adb06709dfb444388350dbaa8e11c48ef7dd69aa0a8950c4c4b86396093c41e04239333d04afbbdaa4bc6'
