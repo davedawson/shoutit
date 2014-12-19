83d525741685c05147f344c0481a5d40db7e29be
davedawson <dave@iamdavedawson.com>
2014-12-19 15:36:47 -0500

    [CSS] Style form pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  # Returns the full title on a per-page basis.       # Documentation comment
+  def full_title(page_title)                          # Method definition
+    base_title = "Shout Club"  # Variable assignment
+    if page_title.empty?                              # Boolean test
+      base_title                                      # Implicit return
+    else
+      "#{base_title} | #{page_title}"                 # String interpolation
+    end
+  end
+  def body_id(page_id)
+      "#{page_id}"
+  end
+
+  def body_class(page_class)
+      "#{page_class}"
+  end
 end
